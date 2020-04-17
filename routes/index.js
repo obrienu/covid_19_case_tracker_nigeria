fbabe5a67f975542c9511415dc71aa649c6e7527
unknown <obrienules@gmail.com>
2020-04-17 15:33:07 +0100

    Add user route and setup authentication

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const controller = require('../controller/index');
-const auth = require('../middleware/auth')
+const auth = require('../middleware/auth');
 
 const router = express.Router();
 /* GET home page. */
