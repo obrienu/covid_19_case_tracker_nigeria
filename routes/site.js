4f3e7f951940155ebffac554be757f5ce889a43b
unknown <obrienules@gmail.com>
2020-04-23 15:03:14 +0100

    Tweak homepage functionality

--- routes/site.js
+++ routes/site.js
@@ -7,6 +7,9 @@ const router = express.Router();
 
 router.get('/', UserController.getHome);
 router.get('/stat', UserController.getStat);
+router.get('/login', UserController.getLogin);
+router.get('/updates', UserController.getPostData);
+router.get('/register', UserController.getRegister);
 
 
 module.exports = router;
