b944a91704604f75a6becb49ff4fbd7f689ad07e
unknown <obrienules@gmail.com>
2020-04-26 20:15:43 +0100

    Add functionality to handle stats page

--- public/javascripts/serviceComponent.js
+++ public/javascripts/serviceComponent.js
@@ -1,17 +1,133 @@
+/* eslint-disable prefer-arrow-callback */
 /* eslint-disable no-undef */
 $(document).ready(() => {
-//  const url = 'https://nigeria-covid19-api.herokuapp.com/api/v1/nigeria/covid-19/';
+  //  const url = 'https://nigeria-covid19-api.herokuapp.com/api/v1/nigeria/covid-19/';
   const url = 'http://localhost:3000/api/v1/nigeria/covid-19/';
 
   const dataService = new DataFetcher(url);
   const displayService = new Interface();
 
   const handleHomePageData = async () => {
-    const { nationalCases, states } = await dataService.getPresentData();
+    const { nationalCases, states } = await dataService.getData('present');
     displayService.populateNatStat(nationalCases);
     displayService.populateStateStat(states);
   };
   if (window.location.pathname === '/') {
     handleHomePageData();
   }
+
+  const fetchPageData = async (page, type) => {
+    let query;
+    let groupQuery;
+    let data;
+    let region;
+    switch (page) {
+      case 'national':
+        if (type === 'range') {
+          const from = $('#nat-stat-from').val();
+          const to = $('#nat-stat-to').val();
+          query = `national?accumulate=true&from=${from}&to=${to}`;
+          groupQuery = `national?accumulate=false&from=${from}&to=${to}`;
+        } else if (type === 'date') {
+          const date = $('#nat-stat-date').val();
+          query = `national?accumulate=true&date=${date}`;
+          groupQuery = `national?accumulate=false&from=2020-01-01&to=${date}`;
+        }
+        data = await dataService.getData(query);
+        groupData = await dataService.getData(groupQuery);
+        return { data, groupData };
+
+      case 'regional':
+        region = $('#reg-name').val();
+        if (type === 'range') {
+          const from = $('#reg-stat-from').val();
+          const to = $('#reg-stat-to').val();
+          query = `region/${region}?accumulate=true&from=${from}&to=${to}`;
+          groupQuery = `region/${region}?accumulate=false&from=${from}&to=${to}`;
+        } else if (type === 'date') {
+          const date = $('#reg-stat-date').val();
+          query = `region/${region}?accumulate=true&date=${date}`;
+          groupQuery = `region/${region}?accumulate=false&from=2020-01-01&to=${date}`;
+        }
+        data = await dataService.getData(query);
+        groupData = await dataService.getData(groupQuery);
+        return { data, groupData };
+
+      case 'state':
+        region = $('#state-name').val();
+        if (type === 'range') {
+          const from = $('#state-stat-from').val();
+          const to = $('#state-stat-to').val();
+          query = `state/${region}?accumulate=true&from=${from}&to=${to}`;
+          groupQuery = `state/${region}?accumulate=false&from=${from}&to=${to}`;
+        } else if (type === 'date') {
+          const date = $('#state-stat-date').val();
+          query = `state/${region}?accumulate=true&date=${date}`;
+          groupQuery = `state/${region}?accumulate=false&from=2020-01-01&to=${date}`;
+        }
+        data = await dataService.getData(query);
+        groupData = await dataService.getData(groupQuery);
+        return { data, groupData };
+      default:
+        return null;
+    }
+  };
+
+  function hideOpenReport(page) {
+    switch (page) {
+      case 'national':
+        $('#national-stat').fadeOut();
+        $('#nat-chart').html('');
+        break;
+      case 'regional':
+        $('#regional-stat').fadeOut();
+        $('#reg-chart').html('');
+        break;
+      case 'state':
+        $('#state-stat').fadeOut();
+        $('#state-chart').html('');
+        break;
+      default:
+    }
+  }
+
+  function handleDataDisplay(result, page) {
+    const { data, groupData } = result;
+    if (data.length > 0) {
+      displayService.populateStat(data[0], page);
+      const d3Data = dataService.d3FormatData(groupData);
+      displayService.generateChart(d3Data, page);
+    } else {
+      hideOpenReport(page);
+      displayService.handleErrorDisplay('No Data Found');
+    }
+  }
+
+  /* NAT PAGE */
+  const natForm = $('#national-form');
+  natForm.on('submit', async function (e) {
+    e.preventDefault();
+    const dataType = $('#nat-search-type').val();
+    const page = 'national';
+    const result = await fetchPageData(page, dataType);
+    handleDataDisplay(result, page);
+  });
+
+  const regionalForm = $('#regional-form');
+  regionalForm.on('submit', async function (e) {
+    e.preventDefault();
+    const dataType = $('#reg-search-type').val();
+    const page = 'regional';
+    const result = await fetchPageData(page, dataType);
+    handleDataDisplay(result, page);
+  });
+
+  const stateForm = $('#state-form');
+  stateForm.on('submit', async function (e) {
+    e.preventDefault();
+    const dataType = $('#state-search-type').val();
+    const page = 'state';
+    const result = await fetchPageData(page, dataType);
+    handleDataDisplay(result, page);
+  });
 });
--- public/javascripts/services.js
+++ public/javascripts/services.js
@@ -1,3 +1,6 @@
+/* eslint-disable consistent-return */
+/* eslint-disable no-shadow */
+/* eslint-disable no-restricted-globals */
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 /* eslint-disable class-methods-use-this */
@@ -14,24 +17,63 @@ class DataFetcher {
     return res.json();
   }
 
-  getPresentData() {
-    const url = `${this.url}present`;
+  async getData(query) {
+    const url = `${this.url + query}`;
     return this.fetcher(url);
+  }
+
+  d3FormatData(data) {
+    const strictIsoParse = d3.utcParse('%Y-%m-%dT%H:%M:%S.%LZ');
+    return data.map(({ cases, date }) => ({ value: cases, date: strictIsoParse(date) }));
   }
 }
 
 class Interface {
   constructor() {
     this.natCases = $('.nat-value');
+    this.regionalCases = $('.reg-value');
+    this.stateCases = $('.state-value');
+    this.natReportSection = $('#national-stat');
+    this.regReportSection = $('#regional-stat');
+    this.stateReportSection = $('#state-stat');
     this.stateContainer = $('#home-state');
+    this.errorDisplay = $('.error');
     this.generateStateHtml = this.generateStateHtml.bind(this);
+    this.handleErrorDisplay = this.handleErrorDisplay.bind(this);
   }
 
-  populateNatStat(data) {
+  handleErrorDisplay(msg) {
+    this.errorDisplay.text(msg);
+    this.errorDisplay.fadeIn();
+    setTimeout(() => {
+      this.errorDisplay.fadeOut();
+    }, 5000);
+  }
+
+  populateStat(data, page) {
+    let stat;
+    switch (page) {
+      case 'national':
+        this.natReportSection.fadeIn();
+        stat = this.natCases;
+        break;
+      case 'regional':
+        stat = this.regionalCases;
+        this.regReportSection.fadeIn();
+        break;
+      case 'state':
+        this.stateReportSection.fadeIn();
+        stat = this.stateCases;
+        break;
+      default:
+        return null;
+    }
     const { cases, discharged, deaths } = data;
-    this.natCases[0].innerText = cases;
-    this.natCases[1].innerText = discharged;
-    this.natCases[2].innerText = deaths;
+    stat[0].innerText = cases;
+    if (page === 'national') {
+      stat[1].innerText = discharged;
+      stat[2].innerText = deaths;
+    }
   }
 
   populateStateStat(states) {
@@ -46,5 +88,78 @@ class Interface {
     return `<div class="box-content state-cases">
     <span>${state}:</span> ${cases}<span></span>
   </div>`;
+  }
+
+  generateChart(data, page) {
+    const margin = ({
+      top: 20, right: 30, bottom: 30, left: 40,
+    });
+
+    const height = 500;
+    const width = 950;
+
+    const x = d3.scaleUtc()
+      .domain(d3.extent(data, (d) => d.date))
+      .range([margin.left, width - margin.right]);
+
+    const y = d3.scaleLinear()
+      .domain([d3.min(data, (d) => d.value), d3.max(data, (d) => d.value)]).nice()
+      .range([height - margin.bottom, margin.top]);
+
+    const line = d3.line()
+      .defined((d) => !isNaN(d.value))
+      .x((d) => x(d.date))
+      .y((d) => y(d.value));
+
+    const xAxis = (g) => g
+      .attr('transform', `translate(0,${height - margin.bottom})`)
+      .call(d3.axisBottom(x).ticks(width / 80).tickSizeOuter(0));
+
+    const yAxis = (g) => g
+      .attr('transform', `translate(${margin.left},0)`)
+      .call(d3.axisLeft(y))
+      .call((g) => g.select('.domain').remove())
+      .call((g) => g.select('.tick:last-of-type text').clone()
+        .attr('x', 3)
+        .attr('text-anchor', 'start')
+        .attr('font-weight', 'bold')
+        .text(data.y));
+
+    let selector;
+    switch (page) {
+      case 'national':
+        selector = '#nat-chart';
+        break;
+      case 'regional':
+        selector = '#reg-chart';
+        break;
+      case 'state':
+        selector = '#state-chart';
+        break;
+      default:
+        return null;
+    }
+    document.querySelector(selector).innerHTML = '';
+    const svg = d3.select(selector)
+      .style('overflow-x', 'scroll')
+      .style('-webkit-overflow-scrolling', 'touch')
+      .append('svg')
+      .attr('viewBox', [0, 0, width, height])
+      .attr('class', 'svg');
+
+    svg.append('g')
+      .call(xAxis);
+
+    svg.append('g')
+      .call(yAxis);
+
+    svg.append('path')
+      .datum(data)
+      .attr('fill', 'none')
+      .attr('stroke', 'steelblue')
+      .attr('stroke-width', 2)
+      .attr('stroke-linejoin', 'round')
+      .attr('stroke-linecap', 'round')
+      .attr('d', line);
   }
 }
